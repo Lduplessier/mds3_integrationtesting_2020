47757dd15936757d8ef784065a2c7fcc7f8923f1
Lduplessier <lucas.duplessier@gmail.com>
2020-01-17 15:37:10 +0100

    update test

--- test/test.js
+++ test/test.js
@@ -24,3 +24,55 @@ const getCurrentCulor = () => {
   return nextCursor > 0 ? `${colorKey}${nextCursor - 1}` : `${colorKey}O`
 }
 // <-- FIN
+
+describe('Color test API', function() {
+  beforeEach((done) => {
+    indexedDB.remove({}, () => {
+      done();
+    });
+  });
+  // 1 
+  it('Response should be 200 if color list returned', function(done) {
+    chai.request('http://localhost:8080')
+    .get('/test')
+    .end(function(err, res) {
+      expect(res).to.have.status(200);
+      res.should.be.json;
+      res.should.be.object;
+      res.should.be.array;
+      done();
+    });
+  });
+  // 2
+  it('Response should be 404 if path is invalid', function(done) {
+    chai.request(server)
+    .get('/index')
+    .end(function (err, res) {
+      expect(res).to.have.status(404);
+      done();
+    });
+  });
+  //3
+  it('Response should be 201 if a new color is added', function(done) {
+    chai.request(server)
+    .send('/index')
+    .end(function(err, res) {
+      expect(res).to.have.status(201);
+      res.should.be.json;
+      res.should.be.array;
+      done();
+    });
+  });
+  // 4
+  it('Response should be 200 if color list is returned with the new ones', function(done) {
+    chai.request(server)
+    .get('/index')
+    .end(function(err, res) {
+      expect(res).to.have.status(200);
+      res.should.be.json;
+      res.should.be.object;
+      res.should.be.array;
+      done();
+  });
+});
+})
